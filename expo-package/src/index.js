b5984b2a34b5b2c4d2cc9d69922ac2438dad1901
Martin Carianni <martin@zuru.tech>
2020-04-13 15:14:12 +0200

    migrated to expo-permissions and expo-image-picker

--- expo-package/src/index.js
+++ expo-package/src/index.js
@@ -1,15 +1,15 @@
 // @flow
 import { registerNativeHandlers } from 'react-native-activity-feed-core';
-import { ImagePicker, Permissions } from 'expo';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+
 registerNativeHandlers({
   pickImage: async () => {
     await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
     return await ImagePicker.launchImageLibraryAsync({
       allowsEditing: false,
       aspect: [4, 3],
     });
   },
 });
-
 export * from 'react-native-activity-feed-core';
